e01c130c3c25447d8dec327d1d19d4e72d8f9c52
limyewjin <limyewjin@users.noreply.github.com>
2023-03-26 02:51:38 +0000

    Add input from user to prove provenance on

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eth_provenance
+
+This project provides a simple way to send empty transactions on the Ethereum network. These empty transactions can be used to prove the provenance of data by creating an immutable record on the blockchain.
--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-from dotenv import load_dotenv
 import os
+from web3 import Web3
+from hexbytes import HexBytes
+from dotenv import load_dotenv
 
 load_dotenv()
-ETH_ACCOUNT = os.environ["ETH_ACCOUNT"]
-ETH_PRIVATE_KEY = os.environ["ETH_PRIVATE_KEY"]
-ETH_HTTP_PROVIDER = os.environ["ETH_HTTP_PROVIDER"]
 
-from web3 import Web3
+ETH_ACCOUNT = os.getenv('ETH_ACCOUNT')
+ETH_PRIVATE_KEY = os.getenv('ETH_PRIVATE_KEY')
+ETH_HTTP_PROVIDER = os.getenv('ETH_HTTP_PROVIDER')
+NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
+CHAIN_ID = 11155111
+
 w3 = Web3(Web3.HTTPProvider(ETH_HTTP_PROVIDER))
 
-print(w3.from_wei(w3.eth.get_balance(ETH_ACCOUNT), 'ether'))
+print(w3.fromWei(w3.eth.getBalance(ETH_ACCOUNT), 'ether'))
 
-def create_provenance(web3, input):
-  transaction= {
-    'to': '0x0000000000000000000000000000000000000000',
-    'from': ETH_ACCOUNT,
-    'value': 0,
-    'data': input.encode('utf-8').hex(),
-    'gas': 200000,
-    'maxFeePerGas': web3.to_wei(100, 'gwei'),
-    'maxPriorityFeePerGas': web3.to_wei(2, 'gwei'),
-    'nonce': 1,
-    'chainId':11155111}
-  signed = web3.eth.account.sign_transaction(transaction, ETH_PRIVATE_KEY)
-  return web3.eth.send_raw_transaction(signed.rawTransaction)
+def create_provenance(web3, input, nonce):
+    transaction = {
+        'to': NULL_ADDRESS,
+        'from': ETH_ACCOUNT,
+        'value': 0,
+        'data': input.encode('utf-8').hex(),
+        'gas': 200000,
+        'maxFeePerGas': web3.toWei(100, 'gwei'),
+        'maxPriorityFeePerGas': web3.toWei(2, 'gwei'),
+        'nonce': nonce,
+        'chainId': CHAIN_ID}
+    signed = web3.eth.account.sign_transaction(transaction, ETH_PRIVATE_KEY)
+    return web3.eth.send_raw_transaction(signed.rawTransaction)
 
-from hexbytes import HexBytes
-print(HexBytes(create_provenance(w3, "this is a test")))
+nonce = w3.eth.get_transaction_count(ETH_ACCOUNT)
+print(nonce)
+while True:
+    input_text = input('What text do you want to prove provenance on? ')
+    print(HexBytes(create_provenance(w3, input_textm nonce)))
+    nonce + 1 
